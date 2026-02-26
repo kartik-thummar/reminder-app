eef9560b7906e1aca420b779d7f1409b85e64e32
Kartik Thummar <kthummar786@gmail.com>
2026-02-26 16:47:11 +0530

    Success response added

--- backend/Utils.py
+++ backend/Utils.py
@@ -1,7 +1,22 @@
 from boto3 import session
+from mypy_boto3_dynamodb import DynamoDBClient
+from http import HTTPStatus
+import json
 
 SESSION = session.Session()
 
 REGION = 'ap-south-1'
-DYNAMODB_CLIENT = SESSION.client(service_name='dynamodb', region_name=REGION)
+SERVICE = 'dynamodb'
+DYNAMODB_CLIENT: DynamoDBClient = SESSION.client(service_name=SERVICE, region_name=REGION)
 
+DYNAMODB_TASK_TABLE = "Reminders"
+
+class Responses():
+
+    @classmethod
+    def success_response(body: dict) -> dict :
+        return {
+            "statusCode": HTTPStatus.OK, 
+            "body": json.dumps(body),
+            "headers": {"Content-Type": "application/json"}
+            }
