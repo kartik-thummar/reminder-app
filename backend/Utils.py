d2929605ef8e99886380f411432f7836fd6b8fd1
Kartik Thummar <kthummar786@gmail.com>
2026-03-09 20:27:03 +0530

    reminder notification deployment code added

--- backend/Utils.py
+++ backend/Utils.py
@@ -1,5 +1,6 @@
 from boto3 import session
 from mypy_boto3_dynamodb import DynamoDBClient
+from mypy_boto3_ses import SESClient
 from http import HTTPStatus
 from os import environ
 import json
@@ -9,6 +10,7 @@ SESSION = session.Session()
 REGION = 'us-east-1'
 SERVICE = 'dynamodb'
 DYNAMODB_CLIENT: DynamoDBClient = SESSION.client(service_name=SERVICE, region_name=REGION)
+SES_CLIENT: SESClient = SESSION.client(service_name="ses", region_name=REGION)
 
 DYNAMODB_TASK_TABLE = environ['REMINDERS_TABLE']
 
